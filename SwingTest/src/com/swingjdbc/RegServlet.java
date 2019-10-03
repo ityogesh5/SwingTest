f0669c27b367e7b0418b8571ff5cc6499d25dea8
Rock_Yogi <ityogesh5@gmail.com>
2019-10-03 12:36:46 +0530

    SwingTese commit

---
+++ SwingTest/src/com/swingjdbc/RegServlet.java
@@ -0,0 +1,58 @@
+package com.swingjdbc;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/RegServlet")
+public class RegServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		response.setContentType("text/html");
+		Connection con;
+		Statement st;
+		ResultSet rs;
+		PrintWriter out = response.getWriter();
+		String gi = request.getParameter("hdnbt");
+		// System.out.println(gi);
+		try {
+			int count = 0;
+			String uname = request.getParameter("username");
+			String email = request.getParameter("email");
+			String phone = request.getParameter("phone");
+			Class.forName("com.mysql.jdbc.Driver");
+			con = DriverManager.getConnection("jdbc:mysql://localhost:3306/nexa_db", "root", "");
+			st = con.createStatement();
+			if (gi.equals("Register")) {
+				st.executeUpdate("insert into register values('" + uname + "','" 
+			+ email + "'," + "'" + phone + "')");
+				st = con.createStatement();
+				out.println("<script type=\"text/javascript\">");
+				out.println("alert('Registered Successfully');");
+				out.println("location='index.html';");
+				out.println("</script>");
+			} else {
+				RequestDispatcher dispatcher = request.getRequestDispatcher("SelectServlet");
+				dispatcher.forward(request, response);
+			}
+		} catch (SQLException | ClassNotFoundException e) {
+			out.print(e);
+
+		}
+
+	}
+
+}
---
+++ SwingTest/src/com/swingjdbc/SelectServlet.java
@@ -0,0 +1,51 @@
+package com.swingjdbc;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/SelectServlet")
+public class SelectServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		response.setContentType("text/html");
+		Connection con;
+		Statement st;
+		ResultSet rs;
+		PrintWriter out = response.getWriter();
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+			con = DriverManager.getConnection("jdbc:mysql://localhost:3306/nexa_db", "root", "");
+			st = con.createStatement();
+			rs = st.executeQuery("select * from register");
+			out.println("<center><table border=1 width=70%>");
+			out.println("<th>Uname</th><th>Email</th><th>Phone no</th>");
+			while (rs.next()) {
+				out.print("<tr>");
+				out.print("<td>" + rs.getString("username") + "</td><td>" 
+				+ rs.getString("email") + "</td><td>"
+						+ rs.getString("phone") + "</td></br>");
+				out.print("</tr>");
+
+			}
+			out.print("</table></center>");
+			con.close();
+		} catch (Exception e) {
+			out.println("Error Occured : " + e);
+
+		}
+
+	}
+
+}
